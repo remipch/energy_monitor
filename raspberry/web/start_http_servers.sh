e3221571090976b624894b9ccd8c5554eeebe2e6
remipch <75692116+remipch@users.noreply.github.com>
2023-12-11 00:39:53 +0100

    web: stop before start servers
    otherwise, if we start servers twice, saved pids are wrong and we cannot
    stop servers

--- raspberry/web/start_http_servers.sh
+++ raspberry/web/start_http_servers.sh
@@ -2,6 +2,8 @@
 
 script_dir=$(dirname "$0")
 
+$script_dir/stop_http_servers.sh
+
 web_interface_folder=$script_dir
 web_interface_port=8000
 
@@ -12,7 +14,6 @@ web_data_port=8001
 # 2>&1 output stdout and stderr
 # tee duplicate to console
 python3 -u -m http.server -d $web_interface_folder --bind 192.168.1.25 $web_interface_port 2>&1 | tee web_interface.log &
-# echo $(jobs -p) > $script_dir/web_interface_pid.txt
 
 python3 -u -m http.server -d $web_data_folder --bind 192.168.1.25 $web_data_port 2>&1 | tee web_data.log &
 
