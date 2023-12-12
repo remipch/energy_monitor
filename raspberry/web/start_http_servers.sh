64c744495d2a48ff6e05c0aee56962ade2e3d9b5
remipch <75692116+remipch@users.noreply.github.com>
2023-12-12 19:15:19 +0100

    web: get current IP address

--- raspberry/web/start_http_servers.sh
+++ raspberry/web/start_http_servers.sh
@@ -10,11 +10,13 @@ web_interface_port=8000
 web_data_folder=$script_dir/data
 web_data_port=8001
 
+ip_address=$(hostname -I | awk '{print $1}')
+
 # -u option force unbuffered output
 # 2>&1 output stdout and stderr
 # tee duplicate to console
-python3 -u -m http.server -d $web_interface_folder --bind 192.168.1.25 $web_interface_port 2>&1 | tee web_interface.log &
+python3 -u -m http.server -d $web_interface_folder --bind $ip_address $web_interface_port 2>&1 | tee web_interface.log &
 
-python3 -u -m http.server -d $web_data_folder --bind 192.168.1.25 $web_data_port 2>&1 | tee web_data.log &
+python3 -u -m http.server -d $web_data_folder --bind $ip_address $web_data_port 2>&1 | tee web_data.log &
 
 echo $(jobs -p) > $script_dir/http_servers_pid.txt
