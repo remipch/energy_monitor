aed3311de27df96647055140faa294e5f61a065c
remipch <75692116+remipch@users.noreply.github.com>
2023-12-12 17:18:45 +0100

    graph_builder: read input from csv instead of parameter
    (simple way to run data_recorder and graph_builder in parallel because
    the later is less important and slow)

--- raspberry/graph_builder.py
+++ raspberry/graph_builder.py
@@ -4,19 +4,37 @@ import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
 import os
 import shutil
+from timeit import timeit
+from collections import deque
+from io import StringIO
+import pandas as pd
 
+import time as t
 
-MAX_DATA_COUNT = 100
+# Input csv must :
+# - respect name pattern: yyyy-mm-dd.csv
+# - only contain values for a single day
+# - have column 'hour' and 'minute'
+# - eventually have column 'second'
 
 class GraphBuilder:
 
     def __init__(self,
+                 input_directory,
+                 line_count, # last lines of input_csv_path to read
+                 columns,    # must match csv header
+                 labels,
                  output_image_path,
                  title = "",
                  ylabel = "",
-                 labels = [],
                  time_label_format = "%Y-%m-%d %H:%M:%S",
                  time_span = timedelta(hours=1)):
+
+        assert len(columns)==len(labels)
+
+        self.input_directory = input_directory + "/"
+        self.line_count = line_count
+        self.columns = columns
         self.output_image_path = output_image_path
         self.time_label_format = time_label_format
         self.time_span = time_span
@@ -27,32 +45,58 @@ class GraphBuilder:
         self.times = []
         self.values = []
 
+        self.modification_time = None
+
         self.fig, self.ax = plt.subplots(figsize=(4, 6), layout="constrained")
         plt.title(title)
         plt.xlabel('Time')
         plt.ylabel(ylabel)
 
-        # Create and store empty curves with labels, their data will be updated in 'add' method
+        # Create and store empty curves with labels, their data will be updated in 'update' method
         self.curves = []
         for l in labels:
             curve, = self.ax.plot([], [], label=l)
             self.curves.append(curve)
         plt.legend()
 
-    def add(self, time, values):
-        assert len(values)==len(self.curves)
 
-        self.times.append(time)
-        self.values.append(values)
+    def __readDayCsvLastLines(self, input_csv_path):
+        with open(input_csv_path , 'r') as f:
+            q = [f.readline()] # read the header
+            q.extend(deque(f, self.line_count))
+            df = pd.read_csv(StringIO('\n'.join(q)))
+            return df
 
-        # Remove old data
-        if len(self.times)>MAX_DATA_COUNT:
-            remove_count = len(self.times) - MAX_DATA_COUNT
-            self.times = self.times[remove_count:]
-            self.values = self.values[remove_count:]
+    # Recompute and save graph is file has been modified
+    def update(self, now):
+        input_csv_path = self.input_directory + now.strftime("%Y-%m-%d") + ".csv"
 
-        max_time = time
-        min_time = time - self.time_span
+        modification_time = os.path.getmtime(input_csv_path)
+
+        if self.modification_time == modification_time:
+            return
+
+        print("Update graph from " + input_csv_path)
+        self.modification_time = modification_time
+
+        # Read last csv lines and add a time column
+        # it's safe to convert to datetime because we have one file per day
+        df = self.__readDayCsvLastLines(input_csv_path)
+        if 'second' in df.columns:
+            df['time'] = df['hour'].astype(str) + ':' + df['minute'].astype(str) + ':' + df['second'].astype(str)
+            df['time'] = pd.to_datetime(df['time'], format='%H:%M:%S') #.dt.time
+        else:
+            df['time'] = df['hour'].astype(str) + ':' + df['minute'].astype(str)
+            df['time'] = pd.to_datetime(df['time'], format='%H:%M') #.dt.time
+
+        max_time = df['time'].max()
+        min_time = max_time - self.time_span
+
+        # Filter rows after min_time
+        filtered_df = df[(df['time'] > min_time)]
+        print(filtered_df)
+        times = filtered_df['time']
+        filtered_df = filtered_df[self.columns]
 
         # Set x axis scale
         mid_time = min_time + (max_time - min_time) / 2
@@ -62,8 +106,8 @@ class GraphBuilder:
         self.ax.set_xlim(min_time, max_time)
 
         # Update curves
-        for i, curve in enumerate(self.curves):
-            curve.set_data(self.times, [v[i] for v in self.values])
+        for column, curve in zip(self.columns, self.curves):
+            curve.set_data(times, filtered_df[column])
 
         # Recompute y autoscale and draw
         self.ax.relim()
--- raspberry/test_graph_builder.py
+++ raspberry/test_graph_builder.py
@@ -2,25 +2,70 @@ import os
 from graph_builder import GraphBuilder
 from datetime import datetime, timedelta
 import tempfile
+import time
 
-IMAGE_PATH = tempfile.gettempdir()+"/test_graph_builder.svg"
+# Manual test for graph_builder
+
+TEMP_DIR = tempfile.gettempdir() + '/'
+
+CSV_PATH_1 = TEMP_DIR + "2023-12-01.csv"
+CSV_PATH_2 = TEMP_DIR + "2023-12-02.csv"
+
+IMAGE_PATH = TEMP_DIR + "test_graph_builder.svg"
 print("IMAGE_PATH : ", IMAGE_PATH )
+
+if os.path.isfile(CSV_PATH_1 ):
+    os.remove(CSV_PATH_1 )
+
+if os.path.isfile(CSV_PATH_2 ):
+    os.remove(CSV_PATH_2 )
 
 if os.path.isfile(IMAGE_PATH ):
     os.remove(IMAGE_PATH )
 
 graph_builder = GraphBuilder(
+    TEMP_DIR,
+    100,
+    ["a", "b"],
+    ["AAA", "BB"],
     IMAGE_PATH,
     "My Title",
     "my y axis",
-    ["Data 0", "Data 1", "Data 2"],
     "%H:%M:%S",
-    timedelta(minutes=2))
+    timedelta(minutes=1))
 
-now = datetime(2023,12,10,15,47,12)
-graph_builder.add(datetime(2023,12,10,15,45,22), [10,10,10]) # too old: will be skiped
-graph_builder.add(datetime(2023,12,10,15,47,31), [12,13,14])
-graph_builder.add(datetime(2023,12,10,15,47,38), [13,14,15])
-graph_builder.add(datetime(2023,12,10,15,48,25), [12,13,14])
-graph_builder.add(datetime(2023,12,10,15,49,11), [13,13,13])
-exit()
+# day 1
+csv_1 = open(CSV_PATH_1, 'a')
+csv_1.write("hour,minute,second,a,b\n");
+csv_1.write("15,45,22,12,13\n")
+csv_1.flush()
+graph_builder.update(datetime(2023,12,1,15,45,22))
+time.sleep(2)
+
+csv_1.write("15,45,25,13,14\n")
+csv_1.flush()
+graph_builder.update(datetime(2023,12,1,15,45,26))
+time.sleep(2)
+
+csv_1.write("15,45,30,12,13\n")
+csv_1.flush()
+graph_builder.update(datetime(2023,12,1,15,45,30))
+time.sleep(2)
+
+# day 2
+csv_2 = open(CSV_PATH_2, 'a')
+csv_2.write("hour,minute,second,a,b\n");
+csv_2.write("0,12,14,21,22\n")
+csv_2.flush()
+graph_builder.update(datetime(2023,12,2,0,12,15))
+time.sleep(2)
+
+csv_2.write("0,12,25,10,10\n")
+csv_2.flush()
+graph_builder.update(datetime(2023,12,2,0,12,28))
+time.sleep(2)
+
+csv_2.write("0,12,35,11,12\n")
+csv_2.flush()
+graph_builder.update(datetime(2023,12,2,0,12,38))
+time.sleep(2)
