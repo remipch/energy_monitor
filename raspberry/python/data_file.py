ba6aa0cfe31cd52b450a169d0de6f8fc9f5a8a10
remipch <75692116+remipch@users.noreply.github.com>
2023-12-22 21:05:47 +0100

    data_file: open and close at each write
    less chances to corrupt opened file when raspberry is powered off

--- raspberry/python/data_file.py
+++ raspberry/python/data_file.py
@@ -8,6 +8,7 @@ class DataFile:
     # File format (csv) :
     # - first line is the header
     # - next lines are data
+    # - end line is "\r\n"
 
     # max_size_bytes: auto remove data files when total data file size is greater
     def __init__(self, directory, header, max_size_bytes = None):
@@ -17,49 +18,24 @@ class DataFile:
             os.makedirs(self.directory)
         self.header = header
         self.max_size_bytes = max_size_bytes
-        self.file = None
-        self.writer = None
-        self.previous_date = None
 
         # Try to repair the last csv file in the data folder
         # csv file can be corrupted if the system has been powered off
         # while DataFile was writing to it
         repair_last_csv_in_directory(directory, len(header))
 
-    def close(self):
-        if self.file is not None:
-            self.file.close()
-            self.file = None
-        self.writer = None
-        self.previous_date = None
-
     def write(self, now, values):
         assert len(values)==len(self.header)
 
         path = self.directory / (now.strftime("%Y-%m-%d") + ".csv")
 
-        if self.file is None:
-            self.__openFile(path)
+        new_file = (not os.path.isfile(path)) or os.stat(path).st_size==0
+        with open(path, 'a') as f:
+            writer = csv.writer(f)
+            if new_file:
+                print("New file created: ", path)
+                writer.writerow(self.header)
+                if self.max_size_bytes is not None:
+                    purge_data_directory(self.directory, self.max_size_bytes)
 
-        elif now.date() != self.previous_date:
-            print("New day: close current file and open new file")
-            self.close()
-            self.__openFile(path)
-
-        # Write and flush
-        self.writer.writerow(values)
-        self.file.flush()
-        self.previous_date = now.date()
-
-    def __openFile(self, path):
-        if os.path.isfile(path):
-            print("Open existing file: ", path)
-            self.file = open(path, 'a', newline='')
-            self.writer = csv.writer(self.file)
-        else:
-            print("Create new file: ", path)
-            if self.max_size_bytes is not None:
-                purge_data_directory(self.directory, self.max_size_bytes)
-            self.file = open(path, 'a', newline='')
-            self.writer = csv.writer(self.file)
-            self.writer.writerow(self.header)
+            writer.writerow(values)
